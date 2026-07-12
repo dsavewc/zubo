fc3f8dcb0a3b4acd75afc7146c40eb360cec93c9
dsavewc <386927390@qq.com>
2026-07-12 11:52:54 +0800

    Update fofa_fetch.py

--- fofa_fetch.py
+++ fofa_fetch.py
@@ -20,6 +20,7 @@ data = {
   "page": 1,
   "page_size": 10
 }
-response = requests.post('https://www.daydaymap.com/api/v1/raymap/search/all', headers=headers, json=data, verify=False, timeout=20)
-    
+r = requests.post('https://www.daydaymap.com/api/v1/raymap/search/all', headers=headers, json=data, verify=False, timeout=20)
+json_data = r.requests.body
+print(json_data)
     
